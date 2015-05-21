086c47789baafb345cde336810027b7f3278dd77
Noah Pederson <pedersonnoah@gmail.com>
2015-05-20 21:57:47 -0500

    Added models for JSON and XML deserialization

---
+++ RunAllBots/Models/KanMusuBotData.cs
@@ -0,0 +1,152 @@
+﻿namespace RedditBots {
+
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    [System.Xml.Serialization.XmlRootAttribute(Namespace = "", IsNullable = false)]
+    public partial class KanMusus {
+
+        private KanMususBot[] botField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("Bot")]
+        public KanMususBot[] Bot {
+            get {
+                return this.botField;
+            }
+            set {
+                this.botField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class KanMususBot {
+
+        private KanMususBotSubreddit[] subredditsField;
+
+        private string[] tagsField;
+
+        private string[] repliesField;
+
+        private string usernameField;
+
+        private string passwordField;
+
+        private byte idField;
+
+        private bool enabledField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlArrayItemAttribute("Subreddit", IsNullable = false)]
+        public KanMususBotSubreddit[] Subreddits {
+            get {
+                return this.subredditsField;
+            }
+            set {
+                this.subredditsField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlArrayItemAttribute("Tag", IsNullable = false)]
+        public string[] Tags {
+            get {
+                return this.tagsField;
+            }
+            set {
+                this.tagsField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlArrayItemAttribute("Reply", IsNullable = false)]
+        public string[] Replies {
+            get {
+                return this.repliesField;
+            }
+            set {
+                this.repliesField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string username {
+            get {
+                return this.usernameField;
+            }
+            set {
+                this.usernameField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public string password {
+            get {
+                return this.passwordField;
+            }
+            set {
+                this.passwordField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public byte id {
+            get {
+                return this.idField;
+            }
+            set {
+                this.idField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public bool enabled {
+            get {
+                return this.enabledField;
+            }
+            set {
+                this.enabledField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true)]
+    public partial class KanMususBotSubreddit {
+
+        private bool searchTitleFlairField;
+
+        private string valueField;
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlAttributeAttribute()]
+        public bool SearchTitleFlair {
+            get {
+                return this.searchTitleFlairField;
+            }
+            set {
+                this.searchTitleFlairField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlTextAttribute()]
+        public string Value {
+            get {
+                return this.valueField;
+            }
+            set {
+                this.valueField = value;
+            }
+        }
+    }
+
+
+
+}
---
+++ RunAllBots/Models/PixivResWorks.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunAllBots.PixivResponse {
+    class Works {
+        
+public string status { get; set; }
+public Response[] response { get; set; }
+public int count { get; set; }
+
+public class Response
+{
+public int id { get; set; }
+public string title { get; set; }
+public string caption { get; set; }
+public string[] tags { get; set; }
+public string[] tools { get; set; }
+public Image_Urls image_urls { get; set; }
+public int width { get; set; }
+public int height { get; set; }
+public object stats { get; set; }
+public int publicity { get; set; }
+public string age_limit { get; set; }
+public string created_time { get; set; }
+public string reuploaded_time { get; set; }
+public User user { get; set; }
+public bool is_manga { get; set; }
+public bool is_liked { get; set; }
+public int favorite_id { get; set; }
+public int page_count { get; set; }
+public string book_style { get; set; }
+public string type { get; set; }
+public object metadata { get; set; }
+public object content_type { get; set; }
+}
+
+public class Image_Urls
+{
+public string small { get; set; }
+}
+
+public class User
+{
+public int id { get; set; }
+public string account { get; set; }
+public string name { get; set; }
+public bool is_following { get; set; }
+public bool is_follower { get; set; }
+public bool is_friend { get; set; }
+public object is_premium { get; set; }
+public Profile_Image_Urls profile_image_urls { get; set; }
+public object stats { get; set; }
+public object profile { get; set; }
+}
+
+public class Profile_Image_Urls
+{
+public string px_50x50 { get; set; }
+}
+
+    }
+}
