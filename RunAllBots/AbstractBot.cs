8dc33e9567b813ea2f62ffacb95d07fa94216de3
Noah Pederson <pedersonnoah@gmail.com>
2015-09-07 23:45:09 -0500

    Added support for querying for and parsing -booru tags through IQDB. DB handling changes

--- RunAllBots/AbstractBot.cs
+++ RunAllBots/AbstractBot.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Web;
 using RedditSharp;
 using RedditSharp.Things;
 using System.Net;
@@ -12,11 +12,11 @@ using System.Globalization;
 using Mono.Data.Sqlite;
 using System.Data;
 using System.Text.RegularExpressions;
-using RunAllBots.PixivAuthentication;
 using Newtonsoft.Json;
+using System.Collections.Specialized;
 
-namespace RunAllBots {
-    abstract class AbstractBot {
+namespace RedditBots {
+    abstract public class AbstractBot {
 
         protected const string BOT_ADMIN = "chiefnoah";
 
@@ -24,12 +24,13 @@ namespace RunAllBots {
         protected Reddit reddit;
         protected AuthenticatedUser user;
 
+        protected const string IQDB = "http://iqdb.org/";
         protected const string PixivBaseUrl = "http://spapi.pixiv.net/iphone/illust.php";
         protected const string PixivPAPIBaseUrl = "https://public-api.secure.pixiv.net/v1";
         protected const string SauceNAOBaseUrl = "http://saucenao.com/search.php";
         protected const string SauceNAOAPIKey = "07c14d9e56055d3f9119b4fba0cef10b42824057";
         protected const string PixivClientID = "bYGKuGVw91e0NMfPGp44euvGt59s";
-        protected const String PixivClientSecret = "HP3RmkgAmEGro0gn1x9ioawQE8WMfvLXDz3ZqxpK";
+        protected const string PixivClientSecret = "HP3RmkgAmEGro0gn1x9ioawQE8WMfvLXDz3ZqxpK";
 
         IDbConnection dbConnection;
 
@@ -37,9 +38,14 @@ namespace RunAllBots {
             reddit = new Reddit();
 
             //Establish a connection to the database
-            dbConnection = (IDbConnection)new SqliteConnection("Data Source=BotData.db,Version=3");
-            dbConnection.Open();
-            InitializeDatabase();
+            try {
+                dbConnection = (IDbConnection)new SqliteConnection("Data Source=BotData.db,Version=3");
+                dbConnection.Open();
+                InitializeDatabase();
+            } catch (SqliteException e) {
+                //We've encountered a sqlite error. We want to fail out because this should break the program.
+                throw e;
+            }
             //TODO: find a way to gaurantee the database connection gets closed
         }
 
@@ -47,7 +53,7 @@ namespace RunAllBots {
         /// The main entry method for a bot. This should return a string for logging.
         /// </summary>
         /// <returns>Formatted string for logging</returns>
-        abstract public String Run();
+        abstract public string Run();
 
 
         /// <summary>
@@ -57,7 +63,7 @@ namespace RunAllBots {
         /// <param name="botName">Name of the bot. This is usually the Reddit username of the bot.</param>
         /// <param name="postID">ID (in base 36) of a Reddit post.</param>
         protected void SavePost(int botId, string botName, string postID) {
-            String sql = "REPLACE INTO BotData (botId, botName, postID) VALUES(" + botId + ", \"" + botName + "\", \"" + postID + "\")";
+            string sql = "REPLACE INTO BotData (botId, botName, postID) VALUES(" + botId + ", \"" + botName + "\", \"" + postID + "\")";
 
             IDbCommand dbcmd = dbConnection.CreateCommand();
             dbcmd.CommandText = sql;
@@ -75,7 +81,7 @@ namespace RunAllBots {
         /// <param name="postID">ID of the Reddit post to search for</param>
         /// <returns></returns>
         protected Boolean CheckIfPostSaved(int botID, string postID) {
-            String sql = "SELECT EXISTS(SELECT * FROM BotData WHERE botId=" + botID + " AND postId =\"" + postID + "\");";
+            string sql = "SELECT EXISTS(SELECT * FROM BotData WHERE botId=" + botID + " AND postId =\"" + postID + "\");";
 
             IDbCommand dbcmd = dbConnection.CreateCommand();
             dbcmd.CommandText = sql;
@@ -111,7 +117,7 @@ namespace RunAllBots {
         /// <param name="postID">ID of the Reddit post to search for</param>
         /// <returns></returns>
         protected Boolean CheckIfPostSaved(string postID) {
-            String sql = "SELECT EXISTS(SELECT * FROM BotData WHERE postId =\"" + postID + "\");";
+            string sql = "SELECT EXISTS(SELECT * FROM BotData WHERE postId =\"" + postID + "\");";
 
             IDbCommand dbcmd = dbConnection.CreateCommand();
             dbcmd.CommandText = sql;
@@ -128,7 +134,7 @@ namespace RunAllBots {
 
                     return true;
                 }
-
+                //TODO: use "using" to handle cleanup. I'm used to Java semantics. Don't hate!
             }
 
             //Perform cleanup
@@ -193,7 +199,7 @@ namespace RunAllBots {
                 if (similarity > 60f) {
                     return (string)o["results"][0]["data"]["pixiv_id"];
                 }
-                retVal += "\r\nCould not find source for: " + imageUrl;
+                retVal += "\r\nCould not find pixiv source for: " + imageUrl;
             } catch (WebException e) {
                 retVal += "SauceNAO Error: " + e.Message;
                 return null;
@@ -209,9 +215,9 @@ namespace RunAllBots {
         /// </summary>
         /// <param name="id"></param>
         /// <returns></returns>
-        protected String[] GetImageTagsByPixivId(string id) {
+        protected List<string> GetImageTagsByPixivId(string id) {
             if (id == null) {
-                return new string[] { };
+                return new List<string>();
             }
             HttpWebRequest req = (HttpWebRequest)HttpWebRequest.Create(PixivPAPIBaseUrl + "/works/" + id + ".json");
             req.Method = "GET";
@@ -228,15 +234,16 @@ namespace RunAllBots {
                 StreamReader reader = new StreamReader(stream, Encoding.UTF8);
                 jsonOutput = reader.ReadToEnd();
             } catch (WebException) {
-                return new string[] { };
+                return new List<string>();
             }
 
-            PixivResponse.Works works = JsonConvert.DeserializeObject<PixivResponse.Works>(jsonOutput);
+            PixivWorksResponse works = JsonConvert.DeserializeObject<PixivWorksResponse>(jsonOutput);
 
-            if (works.status == "success") {
-                return works.response[0].tags;
+            //Now fails out if the image is an album. This will prevent false positives.
+            if (works.status == "success" && works.response[0].page_count < 2) {
+                return works.response[0].tags.ToList();
             }
-            return new string[] { };
+            return new List<string>();
         }
 
 
@@ -248,7 +255,7 @@ namespace RunAllBots {
 
 
             System.Xml.Serialization.XmlSerializer reader = new System.Xml.Serialization.XmlSerializer(typeof(User));
-            String filepath = "./Bots/Data/PixivAuthentication.xml";
+            string filepath = "./Bots/Data/PixivAuthentication.xml";
             System.IO.StreamReader file = new System.IO.StreamReader(filepath);
             User user = new User();
             user = (User)reader.Deserialize(file);
@@ -265,13 +272,81 @@ namespace RunAllBots {
             StreamReader sreader = new StreamReader(stream, Encoding.UTF8);
             string jsonOutput = sreader.ReadToEnd();
 
-            PixivResponse.Auth jsonRes = JsonConvert.DeserializeObject<PixivResponse.Auth>(jsonOutput);
+            PixivAuthResponse jsonRes = JsonConvert.DeserializeObject<PixivAuthResponse>(jsonOutput);
             //Cleanup
 
             res.Close();
             stream.Close();
             sreader.Close();
             return jsonRes.response.access_token;
+        }
+
+        /// <summary>
+        /// Returns a list of -booru tags queried from IQDB with the imageUrl argument
+        /// </summary>
+        /// <param name="imageUrl">URL for the image to query for</param>
+        /// <returns></returns>
+        public List<string> getBooruTags(string imageUrl) {
+            if (imageUrl == null || imageUrl.Length < 1) {
+                return null;
+            }
+            //REGEX: (?<=Tags: )[^\"]*(?=\")
+
+            //Set up the web request
+            HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(IQDB);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.Accept = "*/*";
+            request.Host = "iqdb.org";
+            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.155 Safari/537.36";
+            request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+
+            NameValueCollection headers = new NameValueCollection();
+            headers.Add("Accept-Encoding", "gzip, deflate");
+            headers.Add("Origin", "chrome-extension://aejoelaoggembcahagimdiliamlcdmfm");
+            headers.Add("Accept-Language", "en-US,en;q=0.8,ja;q=0.6");
+
+            //request.Expect = null;
+            request.Headers.Add(headers);
+            
+            //Build the parameters
+            //string postData = "url=" + imageUrl + "&service[]=1&service[]=3&service[]=4&service[]=11";
+            NameValueCollection formData = HttpUtility.ParseQueryString(String.Empty);
+            formData.Add("file", "null");
+            formData.Add("forcegray", "0");
+            formData.Add("url", imageUrl);
+            formData.Add("service[]", "1");
+            formData.Add("service[]", "3");
+            formData.Add("service[]", "4");
+            formData.Add("service[]", "11");
+            string postData = formData.ToString();
+            //Write the parameters to the request stream
+            using (StreamWriter stOut = new StreamWriter(request.GetRequestStream(), System.Text.Encoding.UTF8)) {
+                stOut.Write(postData);
+                stOut.Close(); //I think this is unnecessary because of the using thingy
+            }
+
+            string htmlPage;
+            HttpWebResponse res = (HttpWebResponse)request.GetResponse();
+            using(Stream stream = res.GetResponseStream())
+            using (StreamReader sreader = new StreamReader(stream, Encoding.GetEncoding(res.CharacterSet))) {
+                htmlPage = sreader.ReadToEnd();
+            }
+            //Return an empty list of strings if something happens
+            if (htmlPage.Contains("No relevant matches")) {
+                return new List<string>();
+            }
+            Regex reg = new Regex("(?<=Tags: )[^\"]*(?=\")", RegexOptions.None);
+                Match match = reg.Match(htmlPage);
+                Group group = match.Groups[0];
+                string tagString = match.Value.ToString();
+                char[] delimeter = {' '};
+
+                List<string> tags = tagString.Split(delimeter, StringSplitOptions.RemoveEmptyEntries).ToList();
+                tags = tags.Distinct().ToList();
+
+            
+            return tags;
         }
 
 
@@ -284,7 +359,7 @@ namespace RunAllBots {
                     //Console.WriteLine("Found unread messages\r\nKind: " + m.Kind);
                     //t1 = comment
                     if (m.Kind == "t1") {
-                        var comment = (RedditSharp.Things.Comment)m;
+                        RedditSharp.Things.Comment comment = (RedditSharp.Things.Comment)m;
                         if (!CheckIfPostSaved(botId, comment.Id)) {
                             string message = "From: " + comment.Author +
                                 "\r\n\r\nSubreddit: " + comment.Subreddit +
