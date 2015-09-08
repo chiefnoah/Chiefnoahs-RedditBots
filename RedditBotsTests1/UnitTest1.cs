4ae67368b823842510ad92b6c9e710b98ef56343
Noah Pederson <pedersonnoah@gmail.com>
2015-09-07 23:48:28 -0500

    Initial commit

---
+++ RedditBotsTests1/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RedditBots;
+using System.Collections.Generic;
+
+namespace RedditBotsTests1 {
+    [TestClass]
+    public class AbstractBotTests {
+        [TestMethod]
+        public void BooruTagsTest() {
+            TestBot testBot = new TestBot();
+            List<string> testTags = testBot.getBooruTags("http://cdn.awwni.me/qvm2.jpg");
+            Assert.IsTrue(testTags.Count > 0);
+        }
+    }
+
+    [TestClass]
+    class TestBot : AbstractBot {
+        public override string Run() {
+            throw new NotImplementedException();
+        }
+    }
+}
