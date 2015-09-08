c7f7d633dc55d3f6e0b4c6743c1d49802633c2c2
Noah Pederson <pedersonnoah@gmail.com>
2015-09-07 23:50:17 -0500

    Runs KanMususBots and OneTrueKongouBot

--- RunAllBots/RunAllBots.cs
+++ RunAllBots/RunAllBots.cs
@@ -10,8 +10,12 @@ namespace RedditBots {
         static void Main(string[] args) {
             //AutoTagBot autoTagBot = new AutoTagBot();
             //WriteLog(autoTagBot.Run());
+
             BotKanMusus kanmusu = new BotKanMusus();
-            WriteLog(kanmusu.Run());
+            OneTrueKongouBot oneTrueKongouBot = new OneTrueKongouBot();
+
+            string output = kanmusu.Run() + oneTrueKongouBot.Run(); //lol this is the longest running part of the code
+            WriteLog(output);
         }
 
         private static void WriteLog(string log)
